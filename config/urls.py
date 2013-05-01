46e7e747ba6eda13fe1eb419dc549098635b9794
ekinertac <ekinertac@gmail.com>
2013-05-01 05:54:57 +0300

    easy url structure

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,17 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+def easy(url_str):
+    import re
+    pattern = re.compile(r':\w+/?')
+    matches = pattern.findall(url_str)
+    for match in matches:
+        var = match[1:-1]
+        var_re = r'(?P<%s>.*)/'%var
+        url_str = url_str.replace(match, var_re)
+    url_str += '$'
+    return url_str
+
 urlpatterns = patterns('',
     url(r'^$', 'config.views.index', name='index'),
     # url(r'^project/', include('project.foo.urls')),

1176de5023ab0be1bc9def3110914f98b95fc5d4
ekinertac <ekinertac@gmail.com>
2013-06-09 04:24:03 +0300

    syntax error

--- config/context.py
+++ config/context.py
@@ -3,7 +3,7 @@ from config import settings
 
 def variables(request):
     return {
-        'SITE_NAME' : settings.SITE_NAME
-        'DESCRIPTION' : settings.DESCRIPTION
-        'AUTHOR' : settings.AUTHOR
+        'SITE_NAME' : settings.SITE_NAME,
+        'DESCRIPTION' : settings.DESCRIPTION,
+        'AUTHOR' : settings.AUTHOR,
     }

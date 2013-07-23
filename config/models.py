2084fa064f7e459adb54a74d4c59b2cfcfc6d49b
ekinertac <ekinertac@gmail.com>
2013-07-23 05:11:30 +0300

    Auto superuser variables

--- config/models.py
+++ config/models.py
@@ -1,14 +1,10 @@
 #==================================================
 # This file is not a real model file
 # it only be the creation of the default superuser
-# 
-# I'm really tired of giving admin/admin@admin.com/admin input for 
-# everyime start syncdb command
+#
+# I'm really tired of giving admin/admin@admin.com/admin input for
+# everyime write the './manage.py syncdb' command
 #==================================================
-
-
-
-
 
 
 from django.conf import settings
@@ -21,6 +17,11 @@ from django.db.models import signals
 # Prevent interactive question about wanting a superuser created.  (This code
 # has to go in this otherwise empty "models" module so that it gets processed by
 # the "syncdb" command during database creation.)
+
+username = 'admin'
+password = 'admin'
+email = 'admin@admin.com'
+
 signals.post_syncdb.disconnect(
     create_superuser,
     sender=auth_models,
@@ -33,16 +34,18 @@ def create_testuser(app, created_models, verbosity, **kwargs):
   if not settings.DEBUG:
     return
   try:
-    auth_models.User.objects.get(username='test')
+    auth_models.User.objects.get(username=username)
   except auth_models.User.DoesNotExist:
-    print '*' * 80
-    print '*' * 80
-    print 'Creating test user -- login: admin, password: admin'
-    print '*' * 80
-    print '*' * 80
-    assert auth_models.User.objects.create_superuser('admin', 'admin@admin.com', 'admin')
+    print '=' * 80
+    print 'Creating Administrative Superuser'
+    print '-' * 80
+    print 'Username: %s' % username
+    print 'Password: %s' % password
+    print 'Email:    %s' % email
+    print '=' * 80
+    assert auth_models.User.objects.create_superuser(username, email, password)
   else:
-    print 'Test user already exists.'
+    print 'Superuser already exists.'
 
 signals.post_syncdb.connect(create_testuser,
     sender=auth_models, dispatch_uid='common.models.create_testuser')
